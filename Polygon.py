df173593c2ee16584069342489a568a7c8ebf11f
zaydbannya <zayd.bannya@gmail.com>
2025-01-28 21:19:53 +0100

    polygon

---
+++ Polygon.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+polygon = turtle. Turtle()
+num_sides = 9
+side_length = 70
+angle = 360.0 / num_sides
+for i in range(num_sides):
+ polygon.forward(side_length)
+ polygon.right(angle)
+turtle. done()
